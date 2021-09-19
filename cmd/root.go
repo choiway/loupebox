3e093ec8891b8fa40c836143fd3c961b03e65206
Wayne Choi <waynechoi@gmail.com>
2021-09-18 22:27:14 -0700

    Add handler for add comman

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ var (
 	userLicense string
 
 	rootCmd = &cobra.Command{
-		Use:   "loupebox",
+		Use: "loupebox	",
 		Short: "A generator for Cobra based Applications",
 		Long: `Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
