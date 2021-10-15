5ffca241276ca598e15f9f895143ca4273fa2619
Wayne Choi <waynechoi@gmail.com>
2021-10-15 10:54:30 -0700

    Clean up dsecription

--- cmd/root.go
+++ cmd/root.go
@@ -10,12 +10,9 @@ var (
 	userLicense string
 
 	rootCmd = &cobra.Command{
-		Use: "loupebox",
-		Short: "Photo management utility",
-		Long: `Loupebox is a cli application that helps you aggregate your photos 
-in an organized manner. It helps you avoid copying duplicates and organizes
-your photos by the date they were taken.
-`,
+		Use:   "loupebox",
+		Short: "Photo aggregation utility",
+		Long:  `Loupebox aggregates your photos into a single folder organized by the date taken.`,
 	}
 )
 
